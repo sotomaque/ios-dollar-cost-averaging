3afaefe414cd2532451ffb3b95947b2da21c7d9e
sotomaque <sotomaque@gmail.com>
2021-09-21 12:05:49 -0700

    generate tableview with some prototype cells;

---
+++ ios-dollar-cost-averaging/View/SearchTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchTableViewCell.swift
+//  ios-dollar-cost-averaging
+//
+//  Created by Enrique Sotomayor on 9/21/21.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+}
